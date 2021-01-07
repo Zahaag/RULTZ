8d47b28aff64d8fdbc3fb87ead83c6c3e2949089
Zahaag <takayoshi24@gmail.com>
2021-01-07 23:55:16 +0100

    Podstawa

---
+++ Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿
+namespace RULTZ
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ImageBox = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ImageBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ImageBox
+            // 
+            this.ImageBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ImageBox.Location = new System.Drawing.Point(464, 9);
+            this.ImageBox.Name = "ImageBox";
+            this.ImageBox.Size = new System.Drawing.Size(324, 429);
+            this.ImageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ImageBox.TabIndex = 0;
+            this.ImageBox.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 48);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(335, 39);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.Enter += new System.EventHandler(this.richTextBox1_Enter);
+            this.richTextBox1.Leave += new System.EventHandler(this.richTextBox1_Leave);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Millimeter);
+            this.button1.Location = new System.Drawing.Point(12, 372);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(335, 66);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "EXECUTE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(105, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 15);
+            this.label1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(368, 48);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label2.Size = new System.Drawing.Size(17, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "→";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(368, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "¬";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(389, 72);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "∨";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(391, 51);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(19, 15);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "∧ ";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(358, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 21);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Fun keys";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(12, 113);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(334, 230);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.ImageBox);
+            this.Name = "Form1";
+            this.Text = "RULTZ";
+            ((System.ComponentModel.ISupportInitialize)(this.ImageBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ImageBox;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace RULTZ //RULTZ - Rozwiązywanie Układów Logicznych Za pomocą Tableau
+{    /*Temat: Aplikacja wspomagająca dowodzenie formuł rachunku zdań metodą Tableau
+     * Zadanka:
+     * Dowodzenie formuł
+     * Rysowanie wykresu
+     */
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeControls();
+        }
+        void InitializeControls()
+        {
+            //Label
+            label1.Text = "Formula";
+            label1.Font = new Font("Arial", 24, FontStyle.Bold);
+
+            //richTextBox1
+            richTextBox1.Tag = "Write your formula here....";
+            richTextBox1.Text = richTextBox1.Tag.ToString();
+            richTextBox1.ForeColor = Color.Silver;
+
+            //ImageBox.Image = new Bitmap("D:\\Nude\\02-Kim-Kardashian-Nude-Leaked-Ass.jpg");
+
+        }
+        private void richTextBox1_Enter(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text == richTextBox1.Tag.ToString())
+            {
+                richTextBox1.Text = "";
+                richTextBox1.ForeColor = Color.Black;
+            }
+        }
+
+        private void richTextBox1_Leave(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text.Trim() == "")
+            {
+                richTextBox1.Text = richTextBox1.Tag.ToString();
+                richTextBox1.ForeColor = Color.Silver;
+            }
+        }
+        String conditions(String Text)
+        {
+            if (Text.Length == 1)
+            {
+                return Text;
+            }
+            else if (Text.Contains("∧"))
+            {
+                int Index = Text.IndexOf("∧");
+                String Left = "";
+                String Right = "";
+
+                for (int i = 0; i < Index; i++)
+                {
+                    Left += Text[i];
+                }
+                for (int i = Index; i < Text.Length-1; i++)
+                {
+                    Right += Text[i];
+                }
+                
+            }
+
+            return "Error in checking conditions";
+        }
+        void Tableu()
+        {
+            //Charge the provided text 
+            String Logic_expresion = richTextBox1.Text.ToString();
+            //Step 1: Remove the whitespaces
+            Logic_expresion = Logic_expresion.Replace(" ", "");
+            //Step 2: *nawiasy
+            //Step 3: Show the results
+            richTextBox2.Text = conditions(Logic_expresion);
+
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += "→";
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += "¬";
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += "∨";
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += "∧";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Tableu();
+        }
+    }
+}
